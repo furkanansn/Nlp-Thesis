f096478df3296b43f7d824794f26bd3281abe6ed
aktasceren <aktasceren@outlook.com>
2022-06-05 01:31:25 +0300

    fix bugs

--- SentheticDialogGeneration/Generator.py
+++ SentheticDialogGeneration/Generator.py
@@ -20,7 +20,7 @@ class Generator:
         # answers = df['Answer'].tolist()
         answers = ["adüye n_605 numaralı otobüs gitmektedir"]
         self.getUnits(answers)
-        print(self.questions)
+        print("\n".join(list(self.questions)))
         print(len(self.questions))
 
     def getUnits(self, answers):
@@ -34,8 +34,8 @@ class Generator:
             if self.unit_number is None:
                 self.unit_number = len(s_list)
             elif self.unit_number is not len(s_list):
-                return []
-        return self.getCombinations(s_list)
+                continue
+            self.getCombinations(s_list)
 
     def getCombinations(self,arr):
         temp = permutations(arr, len(arr))
@@ -58,30 +58,20 @@ class Generator:
         for soln in query:
             q_list.append(soln["Q"])
 
+
         for q in q_list:
+            x = None
+            arr_sent = q.split()
+            if len(arr_sent) > 0:
+                isQ = arr_sent[0] + ' ' + arr_sent[1]
+                q_start = self.prolog.query(f"find_Q1(X, '{isQ}').")
+                for soln in q_start:
+                    if soln["X"] is not None:
+                        x = soln["X"]
+            if x is not None:
+                continue
             clean_string = " ".join(q.split())
             self.questions.add(clean_string)
             self.getUnits([clean_string])
-            # if not self.isListEmpty(q2):
-            #     if 'hangisi  nereye  ' in q2[0][0]:
-            #         print(" ".join(q.split()))
-            #         print(q)
-            #         print(q2[0][0], 1234)
-            # for soln in q2:
-            #     print(soln["Q"])
         s_list.append(q_list)
-        # print(s_list)
-        # for q in s_list:
-        #     print(q)
-            # query = self.prolog.query(f"generate_questions('{answer}',Q).")
-            # q_list = []
-            # print('query')
-            # print(query)
-            # for soln in query:
-            #     print('soln')
-            #     print(soln)
-            #     q_list.append(soln["Q"])
-            # s_list.append({
-            #     "units": q_list
-            # })
         return s_list
